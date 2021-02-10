ee39c1ddb2dbbfb2f743827be2476eae999d9451
smgyl <55810966+smgyl@users.noreply.github.com>
2021-02-10 04:16:20 -0800

    Add files via upload

---
+++ mass_spring.cpp
@@ -0,0 +1,97 @@
+/**
+ * @file mass_spring.cpp
+ * Implementation of mass-spring system using Graph
+ *
+ * @brief Reads in two files specified on the command line.
+ * First file: 3D Points (one per line) defined by three doubles
+ * Second file: Tetrahedra (one per line) defined by 4 indices into the point
+ * list
+ */
+ 
+#include "CME212/SFML_Viewer.hpp"
+#include "mass_spring.hpp"
+
+int main(int argc, char** argv)
+{
+  // Check arguments
+  if (argc < 3) {
+    std::cerr << "Usage: " << argv[0] << " NODES_FILE TETS_FILE\n";
+    exit(1);
+  }
+
+  // Construct an empty graph
+  GraphType graph;
+
+  // Create a nodes_file from the first input argument
+  std::ifstream nodes_file(argv[1]);
+  // Interpret each line of the nodes_file as a 3D Point and add to the Graph
+  Point p;
+  std::vector<typename GraphType::node_type> nodes;
+  while (CME212::getline_parsed(nodes_file, p))
+    nodes.push_back(graph.add_node(p));
+
+  // Create a tets_file from the second input argument
+  std::ifstream tets_file(argv[2]);
+  // Interpret each line of the tets_file as four ints which refer to nodes
+  std::array<int,4> t;
+  while (CME212::getline_parsed(tets_file, t)) {
+    graph.add_edge(nodes[t[0]], nodes[t[1]]);
+    graph.add_edge(nodes[t[0]], nodes[t[2]]);
+#if 0
+    // Diagonal edges: include as of HW2 #2
+    graph.add_edge(nodes[t[0]], nodes[t[3]]);
+    graph.add_edge(nodes[t[1]], nodes[t[2]]);
+#endif
+    graph.add_edge(nodes[t[1]], nodes[t[3]]);
+    graph.add_edge(nodes[t[2]], nodes[t[3]]);
+  }
+
+  // HW2 #1 YOUR CODE HERE
+  // Set initial conditions for your nodes, if necessary.
+
+  // Print out the stats
+  std::cout << graph.num_nodes() << " " << graph.num_edges() << std::endl;
+
+  // Launch the Viewer
+  CME212::SFML_Viewer viewer;
+  auto node_map = viewer.empty_node_map(graph);
+
+  viewer.add_nodes(graph.node_begin(), graph.node_end(), node_map);
+  viewer.add_edges(graph.edge_begin(), graph.edge_end(), node_map);
+
+  viewer.center_view();
+
+  // We want viewer interaction and the simulation at the same time
+  // Viewer is thread-safe, so launch the simulation in a child thread
+  bool interrupt_sim_thread = false;
+  auto sim_thread = std::thread([&](){
+
+      // Begin the mass-spring simulation
+      double dt = 0.001;
+      double t_start = 0;
+      double t_end = 5.0;
+
+      for (double t = t_start; t < t_end && !interrupt_sim_thread; t += dt) {
+        //std::cout << "t = " << t << std::endl;
+        symp_euler_step(graph, t, dt, Problem1Force());
+
+        // Update viewer with nodes' new positions
+        viewer.add_nodes(graph.node_begin(), graph.node_end(), node_map);
+        viewer.set_label(t);
+
+        // These lines slow down the animation for small graphs, like grid0_*.
+        // Feel free to remove them or tweak the constants.
+        if (graph.size() < 100)
+          std::this_thread::sleep_for(std::chrono::milliseconds(1));
+      }
+
+    });  // simulation thread
+
+  viewer.event_loop();
+
+  // If we return from the event loop, we've killed the window.
+  interrupt_sim_thread = true;
+  sim_thread.join();
+
+  return 0;
+}
---
+++ mass_spring.hpp
@@ -0,0 +1,87 @@
+/**
+ * @file mass_spring.hpp
+ * Implementation of mass-spring system using Graph
+ */
+
+
+#include <fstream>
+#include <chrono>
+#include <thread>
+
+#include "CME212/Util.hpp"
+#include "CME212/Color.hpp"
+#include "CME212/Point.hpp"
+
+#include "Graph.hpp"
+
+
+// Gravity in meters/sec^2
+static constexpr double grav = 9.81;
+
+/** Custom structure of data to store with Nodes */
+struct NodeData {
+  Point vel;       //< Node velocity
+  double mass;     //< Node mass
+  NodeData() : vel(0), mass(1) {}
+};
+
+// Define the Graph type
+using GraphType = Graph<NodeData>;
+using Node = typename GraphType::node_type;
+using Edge = typename GraphType::edge_type;
+
+
+/** Change a graph's nodes according to a step of the symplectic Euler
+ *    method with the given node force.
+ * @param[in,out] g      Graph
+ * @param[in]     t      The current time (useful for time-dependent forces)
+ * @param[in]     dt     The time step
+ * @param[in]     force  Function object defining the force per node
+ * @return the next time step (usually @a t + @a dt)
+ *
+ * @tparam G::node_value_type supports ???????? YOU CHOOSE
+ * @tparam F is a function object called as @a force(n, @a t),
+ *           where n is a node of the graph and @a t is the current time.
+ *           @a force must return a Point representing the force vector on
+ *           Node n at time @a t.
+ */
+template <typename G, typename F>
+double symp_euler_step(G& g, double t, double dt, F force) {
+  // Compute the t+dt position
+  for (auto it = g.node_begin(); it != g.node_end(); ++it) {
+    auto n = *it;
+
+    // Update the position of the node according to its velocity
+    // x^{n+1} = x^{n} + v^{n} * dt
+    n.position() += n.value().vel * dt;
+  }
+
+  // Compute the t+dt velocity
+  for (auto it = g.node_begin(); it != g.node_end(); ++it) {
+    auto n = *it;
+
+    // v^{n+1} = v^{n} + F(x^{n+1},t) * dt / m
+    n.value().vel += force(n, t) * (dt / n.value().mass);
+  }
+
+  return t + dt;
+}
+
+
+/** Force function object for HW2 #1. */
+struct Problem1Force {
+  /** Return the force applying to @a n at time @a t.
+   *
+   * For HW2 #1, this is a combination of mass-spring force and gravity,
+   * except that points at (0, 0, 0) and (1, 0, 0) never move. We can
+   * model that by returning a zero-valued force. */
+  template <typename NODE>
+  Point operator()(NODE n, double t) {
+    // HW2 #1: YOUR CODE HERE
+    (void) n; (void) t; (void) grav;    // silence compiler warnings
+    return Point(0);
+  }
+};
+
+
+
